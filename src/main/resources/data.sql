8155c81ffd2d747c8ac79b6d224fedd82c1b61b1
telneki <skylight762@gmail.com>
2022-02-01 03:05:40 +0800

    initial commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,5 +16,12 @@ CREATE TABLE IF NOT EXISTS TRANSACTION
     end_time       datetime
 );
 
-insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 1,1,'ivanov',100 );
-insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 2,2,'ivanov',100 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 1,1,'М. Горький',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 2,2,'А. Толстой',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 3,3,'А. Пушкин',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 4,4,'С. Сергеев',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 5,5,'А. Суриков',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 6,6,'А. Фадеев',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 7,7,'Г. Марков',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 8,8,'С. Есенин',0.0 );
+insert into ACCOUNT (account_id, user_id, user_name, sum) values ( 9,9,'А. Азимов',0.0 );
